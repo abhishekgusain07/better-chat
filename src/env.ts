2ffa75978087e92a62ddc2aa5143facc7c80c4b9
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-11 00:36:32 +0530

    feat: payment integerations

---
+++ src/env.ts
@@ -0,0 +1,24 @@
+import { createEnv } from '@t3-oss/env-nextjs'
+import { z } from 'zod'
+import { config } from 'dotenv'
+config()
+
+// Polar API
+// https://docs.polar.sh/api/overview
+
+export const env = createEnv({
+  shared: {},
+  server: {
+    POLAR_ACCESS_TOKEN: z.string(),
+    POLAR_WEBHOOK_SECRET: z.string(),
+  },
+  client: {
+    NEXT_PUBLIC_FRONTEND_BASE_URL: z.string(),
+  },
+  runtimeEnv: {
+    NEXT_PUBLIC_FRONTEND_BASE_URL: process.env.NEXT_PUBLIC_FRONTEND_BASE_URL,
+    POLAR_ACCESS_TOKEN: process.env.POLAR_ACCESS_TOKEN,
+    POLAR_WEBHOOK_SECRET: process.env.POLAR_WEBHOOK_SECRET,
+  },
+  skipValidation: !!process.env.CI || !!process.env.SKIP_ENV_VALIDATION,
+})
