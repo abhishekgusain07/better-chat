8fa995d90d78d52881a243ae301db668014766d2
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-14 23:19:28 +0530

    fix: payments

--- src/lib/polar/polarclient.ts
+++ src/lib/polar/polarclient.ts
@@ -3,5 +3,5 @@ import { Polar } from '@polar-sh/sdk'
 
 export const api = new Polar({
   accessToken: env.POLAR_ACCESS_TOKEN,
-  server: 'production', // Use sandbox for testing purposes - otherwise use 'production' or omit this line
+  server: env.NODE_ENV === 'production' ? 'production' : 'sandbox',
 })
