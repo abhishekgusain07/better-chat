a82ff0dba87d7a14c99cdbf236024fed33f12baa
abhishekgusain07 <abhishek.gusain1007fb@gmail.com>
2025-08-22 00:01:16 +0530

    Add chat schema relations and update exports

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,3 +1,5 @@
 export * from './auth'
 export * from './subscriptions'
 export * from './relations'
+export * from './chat' // Add new chat schema
+export * from './chat-relations' // Add chat relations
